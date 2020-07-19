9389593ab467f175d3ddebccbba2c060c088fde6
wsnk <wsnk00@yandex.com>
2020-07-19 15:08:29 +0300

    Fix go extra target

--- extra/host-go-1.14.1.mk
+++ extra/host-go-1.14.1.mk
@@ -1,6 +1,6 @@
 host-go: $(OUT_DIR)/.config
 	@echo "Build host-go 1.14.1" 
 	$(BOARD_MAKE) \
-		GO_BOOTSTRAP_VERSION=1.14.1 HOST_GO_BOOTSTRAP_HASH_FILE=$(ROOT_DIR)/targets/go-bootstrap.hash \
-		GO_VERSION=1.14.1 HOST_GO_HASH_FILE=$(ROOT_DIR)/targets/go.hash \
+		GO_VERSION=1.14.1 HOST_GO_HASH_FILE=$(ROOT_DIR)/extra/go.hash \
+		GO_BOOTSTRAP_VERSION=1.14.1 HOST_GO_BOOTSTRAP_HASH_FILE=$(ROOT_DIR)/extra/go-bootstrap.hash \
 		host-go
